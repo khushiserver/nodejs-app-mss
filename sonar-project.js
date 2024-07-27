41eeb4ca0153e506b31322cbf94142ab88784a46
khushiserver <zampathkhushi@gmail.com>
2024-07-26 19:47:01 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,6 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.login': 'squ_a8be4a95daa140a83bc71e1f4b0344fbb8726b61',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': '9518954751',
             'sonar.projectVersion':'1.0',
